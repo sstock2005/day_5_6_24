ead39760f112523fe574f34cbe483e112abbba9a
sstock2005 <sstock2005@outlook.com>
2024-05-23 13:29:45 -0500

    upload

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,60 @@
 
-// today we are maintaining global mutable state
+// today we are going to calculate a SHA256 sum of files
 
-use lazy_static::lazy_static;
-use std::sync::Mutex;
+use walkdir::WalkDir;
+use std::fs::File;
+use std::io::{BufReader, Read, Error};
+use std::path::Path;
+use threadpool::ThreadPool;
+use std::sync::mpsc::channel;
+use ring::digest::{Context, Digest, SHA256};
+use text_io::read;
 
-lazy_static! {
-    static ref RINGS_OF_POWER: Mutex<Vec<String>> = Mutex::new(Vec::new());
-}
-
-fn insert(ring: &str) -> Result<(), &'static str> 
+fn compute_sha256<P: AsRef<Path>>(filepath: P) -> Result<(Digest, P), Error> 
 {
-    let mut db = RINGS_OF_POWER.lock().map_err(|_| "Failed to acquire MutexGuard")?;
-    db.push(ring.to_string());
-    Ok(())
+    let mut buffer_reader = BufReader::new(File::open(&filepath)?);
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1024];
+
+    loop 
+    {
+        let count = buffer_reader.read(&mut buffer)?;
+        if count == 0
+        {
+            break;
+        }
+        context.update(&buffer[..count])
+    }
+
+    Ok((context.finish(), filepath))
 }
 
-fn main() -> Result<(), &'static str> {
-    insert("Narya")?;
-    insert("Nenya")?;
-    insert("Vilya")?;
-    {
-        let db = RINGS_OF_POWER.lock().map_err(|_| "Failed to acquire MutexGuard")?;
+fn main() -> Result<(), Error>
+{
+    let thread_pool = ThreadPool::new(num_cpus::get());
 
-        db.iter().enumerate().for_each(|(i, item)| println!("{}: {}", i, item));
-    }
-    insert("The One")?;
-    Ok(())
+    let (tx, rx) = channel();
+
+    println!("Directory:");
+    let directory: String = read!("{}\n");
+
+    for file in WalkDir::new(directory)
+       .follow_links(true)
+       .into_iter()
+       .filter_map(|e| e.ok())
+       .filter(|e| !e.path().is_dir()) {
+           let path = file.path().to_owned();
+           let tx = tx.clone();
+           thread_pool.execute(move || {
+            let digest = compute_sha256(path);
+            tx.send(digest).expect("Could not send data!");
+           });
+       }
+
+       drop(tx);
+       for t in rx.iter(){
+        let (sha, path) = t?;
+        println!("{:?} {:?}", sha, path);
+       }
+       Ok(())
 }
